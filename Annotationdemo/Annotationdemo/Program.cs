0807c0e8e9cd0a709fb9337b04ba66c6f00d529b
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-08 15:13:26 +0530

    [SUraj]Add:Annotation Demo

---
+++ Annotationdemo/Annotationdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnnotationDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AnnotationTest.Display();
+        }
+    }
+}
